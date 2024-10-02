fbd141d1f7a48d2e43693fbb803bb99d0e8cbfb7
João Paulo <165946623+JoaoPaulo-Senai@users.noreply.github.com>
2024-10-02 16:57:57 -0300

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,40 @@
+-- Exercicio 1
+/* select sigla_depto,nome_depto from empresa.departamento */
+
+-- Exercicio 2
+/* select distinct sigla_depto,nome_depto from empresa.departamento */
+
+-- Exercicio 3
+/* select nome_funcionario from empresa.funcionario */
+
+-- Exercicio 4
+/* select distinct nome_funcionario from empresa.funcionario */
+
+-- Exercicio 5
+/* select distinct nome_projeto from empresa.projeto */
+
+-- Exercicio 6
+/* select nome_depto from departamento where qtdfuncionariosdepto < 11 */
+
+-- Exercicio 7
+/* select nome_depto from departamento where qtdfuncionariosdepto >= 15 */
+
+-- Exercicio 8
+/* select nome_funcionario from funcionario where sigla_depto = 'IT' */
+
+-- Exercicio 9
+/* select nome_funcionario, cargo from funcionario order by cargo desc */
+
+-- Exercicio 10
+/* select nome_depto,sigla_depto from departamento order by sigla_depto asc */
+
+-- Exercicio 11
+/* select nome_projeto from projeto order by nome_projeto desc */
+
+-- Exercicio 12
+/* alter table funcionario add column data_admissao date,add column salario decimal(12,2) */
+
+/* select * from funcionario */
+
+-- Exercicio 13
+/*alter table projeto add column duracao_projeto numeric */
